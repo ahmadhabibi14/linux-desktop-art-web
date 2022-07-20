df4528dda4f16d8f89e441eae2651f6057ca54f4
ahmadhabibi14 <ahmadhabibi7159@gmail.com>
2022-07-20 10:50:52 +0800

    Scrolltop

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// Balik Ke Atas
+mybutton = document.getElementById("atas");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+// Akhir Balik Ke Atas
