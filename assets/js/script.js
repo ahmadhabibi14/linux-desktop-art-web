21e93428b19dd81c1d026959549032232058bd16
ahmadhabibi14 <ahmadhabibi7159@gmail.com>
2022-07-20 11:42:50 +0800

    Random Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ window.onscroll = function () {
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.documentElement.scrollTop > 100) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
@@ -14,7 +14,7 @@ function scrollFunction() {
 }
 
 function topFunction() {
-  document.body.scrollTop = 0;
+//   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
 // Akhir Balik Ke Atas
